e76af9e7c82cc872d3b493844b7b0fada40a25d2
dixogc <dianaxhdzg@gmail.com>
2025-07-19 15:15:25 -0500

    Update CitaResponseDTO.java

--- backend/patitas-care/src/main/java/com/kmd/patitas_care/infraestructure/dto/response/CitaResponseDTO.java
+++ backend/patitas-care/src/main/java/com/kmd/patitas_care/infraestructure/dto/response/CitaResponseDTO.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 
 @Data
 @AllArgsConstructor
