74875fc66194d7517ac18d76dd4569689526d3f1
dixogc <dianaxhdzg@gmail.com>
2025-07-19 16:32:09 -0500

    Update CitaResponseDTO.java

--- backend/patitas-care/src/main/java/com/kmd/patitas_care/infraestructure/dto/response/CitaResponseDTO.java
+++ backend/patitas-care/src/main/java/com/kmd/patitas_care/infraestructure/dto/response/CitaResponseDTO.java
@@ -23,7 +23,7 @@ public class CitaResponseDTO {
     private String motivo;
 
     @Schema(description = "Fecha y hora de la cita", example = "2025-07-05T10:30:00")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime fechaHora;
 
     @Schema(description = "Estado de la cita", example = "AGENDADA")
